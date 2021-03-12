579fea16a008addb60566fc7feb49ce99fd8fb66
COSMOS <apple@users.noreply.github.com>
2021-03-13 01:10:48 +0800

    AkiSora 1.00 后端项目创建

---
+++ src/main/java/com/cosmos/AkiSoraApplication.java
@@ -0,0 +1,13 @@
+package com.cosmos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkiSoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkiSoraApplication.class, args);
+    }
+
+}
