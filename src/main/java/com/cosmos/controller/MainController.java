c8d3d321bdafdf168c268e6ff2384b2fc9c2a813
NOCOSMOS <50519740+NOCOSMOS@users.noreply.github.com>
2021-03-29 03:44:32 +0800

    AkiSora 1.03 配置redis,jasypt加密redis配置文件

--- src/main/java/com/cosmos/controller/MainController.java
+++ src/main/java/com/cosmos/controller/MainController.java
@@ -3,6 +3,7 @@ package com.cosmos.controller;
 import com.cosmos.pojo.Trend;
 import com.cosmos.service.TrendService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,33 @@ import java.util.List;
 @RestController
 public class MainController {
     @Autowired
+    private RedisTemplate<String,Object> redisTemplate;
+    @Autowired
     private TrendService trendService;
     @GetMapping("/QueryAllTrend")
     public List<Trend> trend(){
+        System.out.println("QueryAllTrend");
         return trendService.QueryAllTrend();
     }
+    @GetMapping("/QueryFiveTrend")
+    public List<Trend> fiveTrend(){
+        System.out.println("QueryFiveTrend");
+        return trendService.QueryAllTrend();
+    }
+    @GetMapping("/redis/add")
+    public String addRedisData(){
+        Trend trend = new Trend();
+        trend.setName("123");
+        trend.setContent("13523fffvf");
+        redisTemplate.opsForValue().set("tr",trend);
+        redisTemplate.opsForValue().set("ab","tre214nd");
+        return "成功";
+    }
+    @GetMapping("/redis/del")
+    public String delRedisData(){
+        redisTemplate.delete("name");
+        return "成功";
+    }
+
+
 }
